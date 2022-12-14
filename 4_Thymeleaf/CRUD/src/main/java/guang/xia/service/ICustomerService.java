ae475e6f4d53a0b22e6d6f3a79fe69a94c7b2bd1
quanglv96 <quanglv1712@gmail.com>
2022-12-14 09:50:06 +0700

    crud

---
+++ 4_Thymeleaf/CRUD/src/main/java/guang/xia/service/ICustomerService.java
@@ -0,0 +1,17 @@
+package guang.xia.service;
+
+import guang.xia.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
