50f9d23368efd29589b381f789864e1da50ac57b
quanglv96 <quanglv1712@gmail.com>
2022-12-26 13:31:16 +0700

    demo restful

---
+++ 11_webService/QLKH_restful/src/main/java/guang/xia/repository/ICustomerRepository.java
@@ -0,0 +1,9 @@
+package guang.xia.repository;
+
+import guang.xia.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
---
+++ 11_webService/QLKH_restful/src/main/java/guang/xia/service/customer/ICustomerService.java
@@ -0,0 +1,7 @@
+package guang.xia.service.customer;
+
+import guang.xia.model.Customer;
+import guang.xia.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
